75f1c968f98c48bdb5c13c03d53658c0021dc009
calyeet <jackiver46@gmail.com>
2020-09-08 12:35:23 -0400

    hotfix

---
+++ Corium/Assets/ColorLerp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorLerp : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    public Color startColor;
+    public Color endColor;
+    public bool repeat = false;
+    float startTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    
+    void Update()
+    {
+        if (!repeat)
+        {
+            float t = (Time.time - startTime) * speed;
+            GetComponent<Renderer>().material.color = Color.Lerp(startColor, endColor, t);
+            
+        }
+        else
+        {
+            float t = (Mathf.Sin(Time.time - startTime) * speed);
+            GetComponent<Renderer>().material.color = Color.Lerp(startColor, endColor, t);
+
+        }
+    }
+}
